85a1191a3a8b3cdc57befb1ac1838554a6ed574c
hsjeevan <hsjeevan07@gmail.com>
2018-11-24 14:55:19 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-function prepareFood(table_number, Items)
+function prepareFood(tableNum, items, callback) {
+    console.log('Preparing food for item - ', items);
+    callback(tableNum, items);
+}
+
+function serveFood (tableNum, items) {
+    console.log('Serving food for items - ', items)
+} 
+
+var tableNum = 100;
+var items = ["Burger" , "Pasta" , "Coke"];
+prepareFood(tableNum,items,serveFood)
